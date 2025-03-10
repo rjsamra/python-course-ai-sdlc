8b78891906942d528b9c1fde8b9741dff018d0f5
RAJKUMAR SAMRA <rajkumarsamra@gmail.com>
2025-03-10 14:34:14 +0530

    fix for database

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import DeclarativeBase as declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from datetime import datetime
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./flights.db"
